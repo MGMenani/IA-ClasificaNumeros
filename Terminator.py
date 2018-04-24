9371c1cfc83920a010a7572cf3f1e2ae61f1d61f
AndresBolanos <ronald-9797@hotmail.com>
2018-04-23 21:29:22 -0600

    Selección 80% de train y 20% de validación. Eliminación de imágenes escogidas en cada batch.

--- Terminator.py
+++ Terminator.py
@@ -1,7 +1,8 @@
-##################Imports section###################
+################## Imports section ###################
 # -*- coding: cp1252 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 from sklearn.datasets import fetch_mldata
 mnist = fetch_mldata('MNIST original')
@@ -13,24 +14,24 @@ def load_MNIST_Data():
     train_img, test_img, train_lbl, test_lbl = train_test_split(mnist.data, mnist.target, test_size=1/7.0, random_state=0)
     return train_img, train_lbl, test_img, test_lbl
 
-################# Neural sectionz####################
+################# Neural section ####################
 
 
 class Neural_Network(object):
     def __init__(self):
-        self.inputSize = 784
-        self.outputSize = 10
+        self.inputSize = 784                #Imágenes de 28x28 
+        self.outputSize = 10                #números del 0 al 9
         self.hiddenSize1 = 512              #Primera capa de 512
         self.hiddenSize2 = 128              #Segunda capa de 128
 
-        #inicializo el W
+        #inicializo el W con pesos aleatorios 
         self.W1 = np.random.randn(self.inputSize, self.hiddenSize1)     # (784, 512) entrada
         self.W2 = np.random.randn(self.hiddenSize1, self.hiddenSize2)   # (512, 128) primera capa
         self.W3 = np.random.randn(self.hiddenSize2, self.outputSize)    # (128, 10)  segunda capa
         
     def forward(self, X):
         self.z = np.dot(X, self.W1)
-        self.z2 = self.relu(self.z)                     # activation function capa 2
+        self.z2 = self.relu(self.z)                     # activation function capa 1
         self.z3 = np.dot(self.z2,self.W2) 
         self.z4 = self.relu(self.z3)                    # activation function capa 2
         self.z5 = np.dot(self.z4,self.W3)
@@ -42,54 +43,79 @@ class Neural_Network(object):
     def relu(self,x):
         return np.maximum(x, 0, x)
 
-    #Aun no funciona
-    def cross_entropy(predictions, targets, epsilon=1e-12):
-        """
-        Computes cross entropy between targets (encoded as one-hot vectors)
-        and predictions. 
-        Input: predictions (N, k) ndarray
-               targets (N, k) ndarray        
-        Returns: scalar
-        """
-        predictions = np.clip(predictions, epsilon, 1. - epsilon)
-        N = predictions.shape[0]
-        ce = -np.sum(np.sum(targets*np.log(predictions+1e-9)))/N
-        return ce
-
     
-#Así llaman a cross entropy loss en el ejemplo que encontre
-"""
-predictions = np.array([[0.25,0.25,0.25,0.25],
-                        [0.01,0.01,0.01,0.96]])
-targets = np.array([[0,0,0,1],
-                   [0,0,0,1]])
-ans = 0.71355817782  #Correct answer
-x = cross_entropy(predictions, targets)
-print(np.isclose(x,ans))
-"""
+    #https://deepnotes.io/softmax-crossentropy
+    def softmax(self, X):
+        exps = np.exp(X)                      #calcula cada e**Xi (de cada elemento de la matriz)
+        return exps / np.sum(exps)
 
+    def cross_entropy(self,X,y):
+        """
+        X is the output from fully connected layer (num_examples x num_classes)
+        y is labels (num_examples x 1)
+        """
+        m = y.shape[0]
+        p = self.softmax(X)
+        log_likelihood = -np.log(p)#[range(m),y])
+        loss = np.sum(log_likelihood) / m
+        return loss
+
+def getRandomTesting(train_X,train_Y, porcentage):
+    test_data = []
+    test_label = []
+    validation_data = []
+    validation_label = []
+    testDataSelected = random.sample(range(len(train_X)),int(len(train_X)*porcentage))     #Selecciona el 80 por ciento para entrenar
+    test_data = np.take(train_X, testDataSelected, 0)
+    test_label = np.take(train_Y, testDataSelected, 0)
+    validation_data = np.delete(train_X, testDataSelected, 0)
+    validation_label = np.delete(train_Y, testDataSelected, 0)
+    return test_data, test_label, validation_data, validation_label
 
 def Train():
+    cantTrain = 35          #Número de imágenes del train que se usarán como test
+    
     data = load_MNIST_Data()
     train_X = data[0]       #imagenes de entrenamiento (60000)
     train_Y = data[1]       #Labeld de entrenamiento (60000)
-
     test_X = data[2]        #Imagenes de prueba (10000)
     test_Y = data[3]        #Labels de prueba (10000)
 
-    X = train_X[:100]       #Por el momento se toman las 100 primeras imagenes, debe ser aleatorio
-    Y = train_Y[:100]
+    #se calcula el 80 del total de los datos,
+    #retorna una lista con imagenes de train(80%) y sus labels y imagenes de validacion(20%) y sus labels
+    dataPorcentage = getRandomTesting(train_X,train_Y,0.8)
+    train_X = dataPorcentage[0]
+    train_Y = dataPorcentage[1]
+    validation_X = dataPorcentage[2]                    
+    validation_Y = dataPorcentage[3]  
+    
+    trainRandom = random.sample(range(len(train_X)),cantTrain)                       #toma los índices aleatoriamente para las imágenes de testing
+    X = [train_X[i] for i in trainRandom]                                            #Datos de testing con los índices anteriores
+    Y = [train_Y[i] for i in trainRandom]                                            #labels de los datos anteriores
 
-    Y_vectorizado = np.zeros((len(X), len(X[0])))       #Creacion de labels vectorizados para mandarlos a cross-entropy
+    #Elimina las posiciones que ya fueron utilizadas
+    train_X = np.delete(train_X, trainRandom, 0)
+    train_Y = np.delete(train_Y, trainRandom, 0)
+
+    Y_vectorizado = np.zeros((len(Y), 10))              #Creacion de labels vectorizados para mandarlos a cross-entropy (10 columnas->10 clases)
+    #One Hot Encoding
     for i in range(len(Y)):                     
         Y_vectorizado[i][int(Y[i])] = 1                 #Se pone 1.0 en la posicion del vector
 
     NN = Neural_Network()
     output = NN.forward(X)
+    
+    #print Y_vectorizado
+
+    #print "Predicted Output: \n" + str(output) 
+    #print "Actual Output: \n" + str(Y) 
+
 
     #No funciona aun
-    #ce = NN.cross_entropy(output, Y_vectorizado)
-    #print(ce)
+
+    output = output/np.amax(output, axis=0)
+    ce = NN.cross_entropy(output, Y_vectorizado)
+    print ce
 
     
 Train()
