763b317b8e62efcede302c2234c815e38065dbcf
Michael Granados Menani <migramenani@gmail.com>
2018-05-11 00:56:58 -0600

    Tiene menú para clasificar o entrenar. Tiene imágenes hechas a mano para clasificar

--- Terminator.py
+++ Terminator.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import random
 import os.path as path
 import pickle
+from PIL import Image   
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
 from math import sqrt
 from sklearn.preprocessing import normalize 
 
@@ -27,7 +30,7 @@ class Neural_Network(object):
     def __init__(self):
         self.inputSize = 784                #Imágenes de 28x28 
         self.outputSize = 10                #números del 0 al 9
-        self.hiddenSize1 = 512              #Primera capa de 512
+        self.hiddenSize1 = 128              #Primera capa de 512
         self.hiddenSize2 = 128             #Segunda capa de 128
         self.learningRate = 0.0085
         self.probDrop = 0.5                 #Probabilidad de mantener unidad activa en dropout. Más alto = menos drop
@@ -46,12 +49,10 @@ class Neural_Network(object):
     def forward(self, X):
         self.z = np.dot(X, self.W1)
         self.z2 = self.relu(self.z)                                          # activation function capa 1
-        print "VIEJO: ",self.z2
-        self.z2 = self.dropout(self.z2,1)                                    #DropOut a la capa oculta 1
-        print "NUEVO: ",self.z2
+        #self.z2 = self.dropout(self.z2,1)                                    #DropOut a la capa oculta 1
         self.z3 = np.dot(self.z2,self.W2) 
         self.z4 = self.relu(self.z3)                                         # activation function capa 2
-        self.z4 = self.dropout(self.z4,2)                                    #DropOut a la capa oculta 2
+        #self.z4 = self.dropout(self.z4,2)                                    #DropOut a la capa oculta 2
         self.z5 = np.dot(self.z4,self.W3)                                    # final activation function
         output = self.softmax(self.z5)
         return output
@@ -77,16 +78,13 @@ class Neural_Network(object):
             self.maskHidden1 = labelsDrop
         else:
             self.maskHidden2 = labelsDrop
-        return capa
-        
-        
-        
+        return capa     
         
     def relu(self,x):
         return np.maximum(x, 0, x) #x * (x > 0) #np.maximum(x, 0, x)
 
     def derivate_relu(self,x):
-        return 1 * (x > 0) #np.heaviside(x, 0)
+        return np.heaviside(x, 0)#1 * (x > 0) 
         
     #https://deepnotes.io/softmax-crossentropy
     def softmax(self, X):
@@ -96,10 +94,12 @@ class Neural_Network(object):
 ##        exps = np.exp(X) + np.finfo(float).eps                     #calcula cada e**Xi (de cada elemento de la matriz)
 ##        suma = np.sum(exps,1) 
 ##        return exps / suma[:,None]
-        
+
+    def safe_ln(x, minval=0.00001, maxval = 0.1):
+        return np.log(np.clip(x,minval,maxval))
     
     def cross_entropy(self,p,y):
-        return np.mean(np.sum(np.nan_to_num(-y * np.log(p) - (1 - y) * np.log(1 - p)), axis = 1))
+        return np.mean(np.sum(np.nan_to_num(-y * self.safe_ln(p) - (1 - y) * self.safe_ln(1 - p)), axis = 1))
 ##        m = y.shape[0]
 ##        log_likelihood = -np.log(p)
 ##        loss = np.sum(log_likelihood) / m
@@ -122,7 +122,17 @@ class Neural_Network(object):
             pesos = pickle.load(f)
             self.W1 = pesos["W1"]
             self.W2 = pesos["W2"]
-            self.W3 = pesos["W3"]                      
+            self.W3 = pesos["W3"]
+
+    def clasificar(self, X):
+        self.z = np.dot(X, self.W1)
+        self.z2 = self.relu(self.z)                                          # activation function capa 1
+        self.z3 = np.dot(self.z2,self.W2) 
+        self.z4 = self.relu(self.z3)                                         # activation function capa 2
+        self.z5 = np.dot(self.z4,self.W3)                                    # final activation function
+        output = self.softmax(self.z5)
+        return np.argmax(output)
+        
 
 def getRandomTesting(train_X,train_Y, porcentage):
     test_data = []
@@ -151,69 +161,108 @@ def OneHotEncode(Y):
     return Y_vectorizado
     
 def Train():
+    loss = []
+    accuracy = []
+    
     cantTrain = 32        #Número de imágenes del train que se usarán como test
     data = load_MNIST_Data()
     train_X = data[0]       #imagenes de entrenamiento (60000)
     train_Y = data[1]       #Labeld de entrenamiento (60000)
+
     test_X = data[2]        #Imagenes de prueba (10000)
     test_Y = data[3]        #Labels de prueba (10000)
     
     #se calcula el 80 del total de los datos,
     #retorna una lista con imagenes de train(80%) y sus labels y imagenes de validacion(20%) y sus labels
-    dataPorcentage = getRandomTesting(train_X,train_Y,0.8)
-
-    train_X_aux = dataPorcentage[0]
-    train_Y_aux = dataPorcentage[1]
-    
-    validation_X = dataPorcentage[2]                    
-    validation_Y = dataPorcentage[3] 
-    epocs = 1
+    epocs = 30
     NN = Neural_Network()
-    for i in range(epocs):
-        print("EPOC #"+str(i))
-        train_X = train_X_aux
-        train_Y = train_Y_aux
-        
-        for i in range(1500):
-            trainRandom = random.sample(range(len(train_X)),cantTrain)                   #toma los índices aleatoriamente para las imágenes de training
-            X = np.array([train_X[i] for i in trainRandom]) / 255                            #Datos de testing con los índices anteriores
-            Y = [train_Y[i] for i in trainRandom]                                            #labels de los datos anteriores
 
-            #Elimina las posiciones que ya fueron utilizadas
-            train_X = np.delete(train_X, trainRandom, 0)
-            train_Y = np.delete(train_Y, trainRandom, 0)
+    while (True):
+        opcion = menu()
 
-            #OneHotEncode
-            NN.y = OneHotEncode(Y)
-        
-            output = NN.forward(X)
-            
-            #print("Exactitud "+str(getAccuracy(output,NN.y)))
-            #print("Forward")
-            #print(output)
+        if opcion == 1:
+            for j in range(epocs):
+                dataPorcentage = getRandomTesting(train_X,train_Y,0.8)
 
-            ce = NN.cross_entropy(output, NN.y)
-            #print ce
-            
-            NN.backward(X, ce, output)
-
-        print("Loss "+str(ce))
-        print("Exactitud "+str(getAccuracy(output,NN.y)))
-
-        NN.guardarPesos()
-
-
-    test_X  = test_X / 255
-    NN.y = OneHotEncode(test_Y)
-    print("Analisis final")
-    output = NN.forward(test_X)
-    ce = NN.cross_entropy(output, NN.y)
-    print("Loss "+str(ce))
-    print("Exactitud "+str(getAccuracy(output,NN.y)))
+                train_X_aux = dataPorcentage[0]
+                train_Y_aux = dataPorcentage[1]
     
+                validation_X = dataPorcentage[2]                    
+                validation_Y = dataPorcentage[3]
+        
+                print("EPOC #"+str(j))
+                cont = 0
+                for k in range(1500):
+                    cont+=1
+                    trainRandom = random.sample(range(len(train_X_aux)),cantTrain)                   #toma los índices aleatoriamente para las imágenes de training
+                    X = np.array([train_X_aux[i] for i in trainRandom]) / 255                            #Datos de testing con los índices anteriores
+                    Y = [train_Y_aux[i] for i in trainRandom]                                            #labels de los datos anteriores
+
+                    #Elimina las posiciones que ya fueron utilizadas
+                    train_X_aux = np.delete(train_X_aux, trainRandom, 0)
+                    train_Y_aux = np.delete(train_Y_aux, trainRandom, 0)
+
+                    #OneHotEncode
+                    NN.y = OneHotEncode(Y)
+        
+                    output = NN.forward(X)
+
+                    ce = NN.cross_entropy(output, NN.y)
+                    #print ce
+            
+                    NN.backward(X, ce, output)
+
+                    if (k%100 == 0):
+                        #validation_X  = validation_X / 255
+                        NN.y = OneHotEncode(validation_Y)
+                        output = NN.forward(validation_X)
+                        ce = NN.cross_entropy(output, NN.y)
+                        efectividad = getAccuracy(output,NN.y)
+                        loss += [ce]
+                        accuracy += [efectividad]
+                        print("Loss "+str(ce))
+                        print("Exactitud "+str(getAccuracy(output,NN.y)))
+
+                NN.guardarPesos()
+
+            test_X  = test_X 
+            NN.y = OneHotEncode(test_Y)
+            print("Analisis")
+            output = NN.forward(test_X)
+            ce = NN.cross_entropy(output, NN.y)
+            efectividad = getAccuracy(output,NN.y)
+            loss += [ce]
+            accuracy += [efectividad]
+            print("Loss "+str(ce))
+            print("Exactitud "+str(getAccuracy(output,NN.y)))
+
+            plt.figure(1)
+            plt.subplot(211)
+            plt.plot(loss)
+    
+            plt.subplot(212)
+            plt.plot(accuracy)
+            plt.show()
+        
+        elif opcion == 2:                                      #Clasificar imágenes hechas a mano
+            Tk().withdraw()
+            pathString = askopenfilename()
+            if pathString != "":
+                f = Image.open(pathString)
+                image = f.convert('L')                           # convert image to monochrome
+                image = np.array(image).ravel()
+                print "Se clasificó un ",NN.clasificar(image)
+
+        else:
+            print "Opción incorrecta\n"
 
 
-
+def menu():
+    print "------- MENÚ -------"
+    print "1) Entrenar modelo"
+    print "2) Clasificar"
+    print "Elija una opción:"
+    return input()
     
 Train()
         
